8db670d06bfb875653761cbe1037a73382fc495b
GermainZ <germanosz@gmail.com>
2014-06-22 17:55:26 +0300

    Add blacklist

---
+++ src/com/mohammadag/headsupenabler/SettingsHelper.java
@@ -0,0 +1,68 @@
+package com.mohammadag.headsupenabler;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.robv.android.xposed.XSharedPreferences;
+
+public class SettingsHelper {
+    private static final String PACKAGE_NAME = "com.mohammadag.headsupenabler";
+    private static final String PREFS = PACKAGE_NAME + "_preferences";
+    private static final String BLACKLIST = "blacklist";
+    private XSharedPreferences mXSharedPreferences = null;
+    private SharedPreferences mSharedPreferences = null;
+    private Context mContext = null;
+    private Set<String> mListItems;
+
+    // Called from module's classes.
+    public SettingsHelper() {
+        mXSharedPreferences = new XSharedPreferences(PACKAGE_NAME, PREFS);
+        mXSharedPreferences.makeWorldReadable();
+    }
+
+    // Called from activities.
+    public SettingsHelper(Context context) {
+        mSharedPreferences = context.getSharedPreferences(PREFS, Context.MODE_WORLD_READABLE);
+        mContext = context;
+    }
+
+    public void reload() {
+        if (mXSharedPreferences.hasFileChanged()) {
+            mXSharedPreferences.reload();
+            mListItems = getListItems();
+        }
+    }
+
+    public boolean addListItem(String listItem) {
+        mListItems.add(listItem);
+        SharedPreferences.Editor prefEditor = mSharedPreferences.edit();
+        prefEditor.putStringSet(BLACKLIST, mListItems);
+        prefEditor.apply();
+        return true;
+    }
+
+    public void removeListItem(String listItem) {
+        SharedPreferences.Editor prefEditor = mSharedPreferences.edit();
+        mListItems.remove(listItem);
+        prefEditor.putStringSet(BLACKLIST, mListItems);
+        prefEditor.apply();
+    }
+
+    public boolean isListed(String s) {
+        if (mListItems == null)
+            mListItems = getListItems();
+        return mListItems.contains(s);
+    }
+
+    public Set<String> getListItems() {
+        Set<String> set = new HashSet<String>();
+        if (mSharedPreferences != null)
+            return mSharedPreferences.getStringSet(BLACKLIST, set);
+        else if (mXSharedPreferences != null)
+            return mXSharedPreferences.getStringSet(BLACKLIST, set);
+        return set;
+    }
+}
--- src/com/mohammadag/headsupenabler/XposedMod.java
+++ src/com/mohammadag/headsupenabler/XposedMod.java
@@ -3,6 +3,7 @@ package com.mohammadag.headsupenabler;
 import android.content.Context;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -10,6 +11,8 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedMod implements IXposedHookLoadPackage {
+	private static final SettingsHelper mSettingsHelper = new SettingsHelper();
+
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
@@ -17,14 +20,26 @@ public class XposedMod implements IXposedHookLoadPackage {
 
 		Class<?> BaseStatusBar = XposedHelpers.findClass("com.android.systemui.statusbar.BaseStatusBar", lpparam.classLoader);
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "shouldInterrupt", StatusBarNotification.class,
-				XC_MethodReplacement.returnConstant(true));
+				new XC_MethodReplacement() {
+					@Override
+					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+						String packageName = ((StatusBarNotification) param.args[0]).getPackageName();
+						mSettingsHelper.reload();
+						if (mSettingsHelper.isListed(packageName))
+							return false;
+						return true;
+					}
+				}
+		);
 
 		/* Users won't use adb or terminal, force it upon that */
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "start", new XC_MethodHook() {
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				Context mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
 				Settings.Global.putInt(mContext.getContentResolver(), "heads_up_enabled", 1);
-			};
+			}
+
+			;
 		});
 	}
 }
