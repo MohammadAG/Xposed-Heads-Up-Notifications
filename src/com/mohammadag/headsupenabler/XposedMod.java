5d809281f1aa92fbbb7a62d51215f10a6f5c0545
GermainZ <germanosz@gmail.com>
2014-06-22 19:31:04 +0300

    Allow one finger expansion

--- src/com/mohammadag/headsupenabler/XposedMod.java
+++ src/com/mohammadag/headsupenabler/XposedMod.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.PowerManager;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
+import android.view.MotionEvent;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -11,15 +12,18 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
 
 public class XposedMod implements IXposedHookLoadPackage {
-	private static final SettingsHelper mSettingsHelper = new SettingsHelper();
-
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
+
+		final SettingsHelper mSettingsHelper = new SettingsHelper();
 
 		Class<?> BaseStatusBar = XposedHelpers.findClass("com.android.systemui.statusbar.BaseStatusBar", lpparam.classLoader);
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "shouldInterrupt", StatusBarNotification.class,
@@ -33,6 +37,27 @@ public class XposedMod implements IXposedHookLoadPackage {
 					}
 				}
 		);
+
+		Class<?> HeadsUpNotificationView = XposedHelpers.findClass("com.android.systemui.statusbar.policy.HeadsUpNotificationView",
+				lpparam.classLoader);
+		XposedHelpers.findAndHookMethod(HeadsUpNotificationView, "onAttachedToWindow", new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				setAdditionalInstanceField(getObjectField(param.thisObject, "mExpandHelper"), "headsUp", true);
+			}
+		});
+
+		Class<?> ExpandHelper = XposedHelpers.findClass("com.android.systemui.ExpandHelper", lpparam.classLoader);
+		XposedHelpers.findAndHookMethod(ExpandHelper, "onInterceptTouchEvent", MotionEvent.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				int action = ((MotionEvent) param.args[0]).getAction();
+				Object headsUp = getAdditionalInstanceField(param.thisObject, "headsUp");
+				if (headsUp != null && (action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
+					setObjectField(param.thisObject, "mWatchingForPull", true);
+				}
+			}
+		});
 
 		/* Users won't use adb or terminal, force it upon that */
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "start", new XC_MethodHook() {
