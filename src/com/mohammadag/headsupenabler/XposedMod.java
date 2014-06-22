565ba13c4ac2c462539463be63a62c81a789f3ad
GermainZ <germanosz@gmail.com>
2014-06-22 18:12:04 +0300

    Do nothing if screen is off

--- src/com/mohammadag/headsupenabler/XposedMod.java
+++ src/com/mohammadag/headsupenabler/XposedMod.java
@@ -1,6 +1,7 @@
 package com.mohammadag.headsupenabler;
 
 import android.content.Context;
+import android.os.PowerManager;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 
@@ -9,6 +10,8 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 public class XposedMod implements IXposedHookLoadPackage {
 	private static final SettingsHelper mSettingsHelper = new SettingsHelper();
@@ -25,9 +28,8 @@ public class XposedMod implements IXposedHookLoadPackage {
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 						String packageName = ((StatusBarNotification) param.args[0]).getPackageName();
 						mSettingsHelper.reload();
-						if (mSettingsHelper.isListed(packageName))
-							return false;
-						return true;
+						PowerManager powerManager = (PowerManager) getObjectField(param.thisObject, "mPowerManager");
+						return powerManager.isScreenOn() && !mSettingsHelper.isListed(packageName);
 					}
 				}
 		);
@@ -35,7 +37,7 @@ public class XposedMod implements IXposedHookLoadPackage {
 		/* Users won't use adb or terminal, force it upon that */
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "start", new XC_MethodHook() {
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				Context mContext = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
+				Context mContext = (Context) getObjectField(param.thisObject, "mContext");
 				Settings.Global.putInt(mContext.getContentResolver(), "heads_up_enabled", 1);
 			}
 
