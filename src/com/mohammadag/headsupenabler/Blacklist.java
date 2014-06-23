a55bc93ffb115d513d5bcd0b81a9caaa5df34021
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-06-23 12:26:32 +0300

    Removed unused import

--- src/com/mohammadag/headsupenabler/Blacklist.java
+++ src/com/mohammadag/headsupenabler/Blacklist.java
@@ -19,7 +19,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
