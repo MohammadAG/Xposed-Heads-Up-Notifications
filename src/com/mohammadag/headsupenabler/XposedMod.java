4b83a3c1778aaf021b66f21e7536497aa6fe5624
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-06-23 12:26:02 +0300

    Use a broadcast receiver to clean up after uninstallation

--- src/com/mohammadag/headsupenabler/XposedMod.java
+++ src/com/mohammadag/headsupenabler/XposedMod.java
@@ -1,11 +1,18 @@
 package com.mohammadag.headsupenabler;
 
+import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.PowerManager;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 import android.view.MotionEvent;
-
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -14,18 +21,18 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
-import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
-import static de.robv.android.xposed.XposedHelpers.setObjectField;
-
 public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
-	private static final SettingsHelper mSettingsHelper = new SettingsHelper();
+	private static SettingsHelper mSettingsHelper;
+	private BroadcastReceiver mBroadcastReceiver;
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
+
+		if (mSettingsHelper == null) {
+			mSettingsHelper = new SettingsHelper();
+		}
 
 		Class<?> BaseStatusBar = XposedHelpers.findClass("com.android.systemui.statusbar.BaseStatusBar", lpparam.classLoader);
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "shouldInterrupt", StatusBarNotification.class,
@@ -67,7 +74,29 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 		XposedHelpers.findAndHookMethod(BaseStatusBar, "start", new XC_MethodHook() {
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				Context mContext = (Context) getObjectField(param.thisObject, "mContext");
+				mBroadcastReceiver = new BroadcastReceiver() {
+					@Override
+					public void onReceive(Context context, Intent intent) {
+						if (intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) {
+							return;
+						}
+
+						// User is uninstalling us, NOOOOOOOOOOOOOOOOOOO
+						Log.d("Xpsoed", "Cleaning up after Heads Up uninstallation");
+						Settings.Global.putInt(context.getContentResolver(), "heads_up_enabled", 0);
+					}
+				};
+
+				mContext.registerReceiver(mBroadcastReceiver, new IntentFilter(Intent.ACTION_PACKAGE_REMOVED));
 				Settings.Global.putInt(mContext.getContentResolver(), "heads_up_enabled", 1);
+			}
+		});
+
+		XposedHelpers.findAndHookMethod(BaseStatusBar, "destroy", new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				Context mContext = (Context) getObjectField(param.thisObject, "mContext");
+				mContext.unregisterReceiver(mBroadcastReceiver);
 			}
 		});
 	}
@@ -76,6 +105,11 @@ public class XposedMod implements IXposedHookLoadPackage, IXposedHookInitPackage
 	public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
 		if (!resparam.packageName.equals("com.android.systemui"))
 			return;
+
+		if (mSettingsHelper == null) {
+			mSettingsHelper = new SettingsHelper();
+		}
+
 		resparam.res.setReplacement("com.android.systemui", "integer", "heads_up_notification_decay",
 				mSettingsHelper.getHeadsUpNotificationDecay());
 	}
